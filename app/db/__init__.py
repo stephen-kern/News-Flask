246cbfeced39e3f3409d16dbd6676bc712580a13
stephen <94320530+stephen-kern@users.noreply.github.com>
2023-03-02 16:19:50 -0600

    updated routes and created utils/filters

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,15 +3,34 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
 # connect to the database using the env variable
-engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
-
 # The engine variable manages the overall connection to the database.
 # The Session variable generates temporary connections for performing
 # create, read, update, and delete (CRUD) operations.
 # The Base class variable helps us map the models to real MySQL tables.
+engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def get_db():
+    if 'db' not in g: 
+        #store db connection in app context
+        g.db = Session()
+        
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db(app):
+    Base.metadata.create_all(engine)
+
+    app.teardown_appcontext(close_db)
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,21 +1,36 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
-# Blueprint() lets us consolidate routes onto a single bp object that the parent app can register later. 
+# Blueprint() lets us consolidate routes onto a single bp object that the parent app can register later.
 bp = Blueprint('home', __name__, url_prefix='/')
 
-# We then define two new functions: index() and login(). 
-# In each case, we add a @bp.route() decorator before the 
-# function to turn it into a route. Whatever the function 
-# returns becomes the response. And this time, 
+# We then define two new functions: index() and login().
+# In each case, we add a @bp.route() decorator before the
+# function to turn it into a route. Whatever the function
+# returns becomes the response. And this time,
 # we use the render_template() function to respond with a template instead of a string.
+
+
 @bp.route('/')
 def index():
-    return render_template('homepage.html')
+    # get all posts
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+
+    return render_template('homepage.html', posts=posts)
+
 
 @bp.route('/login')
 def login():
     return render_template('login.html')
 
+
 @bp.route('/post/<id>')
 def single(id):
-    return render_template('single-post.html')
+    # get single post by id
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+
+    # render single post page
+    return render_template('single-post.html', post=post)
